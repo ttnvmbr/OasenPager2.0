d2be5f742261f03a18f06c01a0f62fb2f355390c
Inci <inciyesiltepe@gmail.com>
2022-04-04 20:23:02 +0200

    new build

--- oasenpager/remix.config.js
+++ oasenpager/remix.config.js
@@ -2,10 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "netlify",
+  server: "./server.js",
   ignoredRouteFiles: [".*"],
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  publicPath: "/build/",
-  devServerPort: 8002,
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
+  // publicPath: "/build/",
 };
