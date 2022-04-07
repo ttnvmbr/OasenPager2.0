cf331b46a39240f1def448d3b70c7dd048013f33
Inci <inciyesiltepe@gmail.com>
2022-04-07 20:15:17 +0200

    icons fix

--- oasenpager/app/components/Footer/Component.jsx
+++ oasenpager/app/components/Footer/Component.jsx
@@ -20,7 +20,7 @@ export function Footer() {
               >
                 <path
                   fill="white"
-                  fillRule="evenodd"
+                  fill-rule="evenodd"
                   d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z"
                   clipRule="evenodd"
                 />
@@ -34,7 +34,7 @@ export function Footer() {
             >
               <span
                 className="iconify  text-5xl md:text-6xl"
-                dataicon="entypo-social:google-drive"
+                data-icon="entypo-social:google-drive"
               ></span>
             </a>
           </li>
@@ -43,10 +43,7 @@ export function Footer() {
               href="https://trello.com/b/gJG5AL7e/oasen"
               className="text-xl oasenwhite hover:text-black duration-500"
             >
-              <span
-                className="iconify text-5xl md:text-6xl"
-                dataicon="bxl:trello"
-              ></span>
+              <span className="iconify text-7xl" data-icon="bxl:trello"></span>
             </a>
           </li>
           <li className="md:w-14 w-10">
@@ -56,7 +53,7 @@ export function Footer() {
             >
               <span
                 className="iconify text-4xl md:text-6xl"
-                dataicon="simple-icons:miro"
+                data-icon="simple-icons:miro"
               ></span>
             </a>
           </li>
